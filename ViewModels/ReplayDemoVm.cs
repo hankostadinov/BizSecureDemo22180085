8d2a7a00542fa4dbf0d4cb6cc49c18e71c550909
bkostadinov_22180085@unwe.bg <bkostadinov_22180085@unwe.bg>
2026-03-18 14:43:50 +0200

    Replay attack detected 302

---
+++ ViewModels/ReplayDemoVm.cs
@@ -0,0 +1,15 @@
+﻿namespace BizSecureDemo22180085.ViewModels
+{
+    public class ReplayDemoVm
+    {
+        public decimal Balance { get; set; }
+        public string? Message { get; set; }
+        public int UserId { get; set; }
+        public decimal Amount { get; set; } = 100;
+        public string Token { get; set; } = "SECRET123";
+
+        public string Nonce { get; set; } = string.Empty;
+
+
+    }
+}
